9a2d3c5e4d4324b0e703b73dcd43a3783c548630
Jeyadev <73011515+Jeyadev-Muthukumarasamy@users.noreply.github.com>
2025-06-27 10:31:12 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,41 +1,32 @@
-  import express, { Request, Response } from "express";
-  import mongoose from "mongoose";
-  import { router } from "./src/View/view";
-  import cors from "cors";
+import mongoose from "mongoose";
+import {router} from "./src/View/view"
+import express,{Request,Response} from "express";
+import cors from 'cors'
+import dotenv from 'dotenv'
 
-  const app = express();
+dotenv.config()
+const app = express();
+app.use(cors());
 
-  // Middleware to parse incoming requests with JSON payloads
-  app.use(cors());
+app.use(express.json());
 
- 
-  app.use(express.json());
+const MONGODB_URI = process.env.MONGODB_URI;
 
-  // CORS configuration
+if (!MONGODB_URI) {
+  console.error("MongoDB connection URI not found in environment variables");
+  process.exit(1);
+}
 
-
-
-
-  // Route to test if the server is working
-  app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "hi" });
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => {
+    console.error("Connection error:", err);
+    process.exit(1);
   });
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hi" });
+});
+app.use("/api", router);
 
-  // MongoDB connection
-  mongoose
-    .connect('mongodb+srv://jeydev007:uWGTs9KCMWRrcpCO@cluster0.opvwi.mongodb.net/')
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch(err => {
-      console.error('Connection error:', err);
-      process.exit(1); // Exit the process if the connection fails
-    });
-
-  // Use the router for API routes
-  app.use("/api", router);
-
-
-
-  // Start the server
-  app.listen(3000, () => {
-    console.log("Server started on port 3000");
-  });
+export default app
